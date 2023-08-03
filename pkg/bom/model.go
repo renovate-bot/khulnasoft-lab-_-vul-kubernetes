2b55321bf8b9447904a16c31e5292da34af4a19c
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-03 17:26:06 +0600

    Create model.go

---
+++ pkg/bom/model.go
@@ -0,0 +1,33 @@
+package bom
+
+type Result struct {
+	ID         string      `json:"name"`
+	Type       string      `json:"type,omitempty"`
+	Components []Component `json:"components,omitempty"`
+	NodesInfo  []NodeInfo  `json:"nodesInfo,omitempty"`
+}
+
+type Component struct {
+	Namespace  string
+	Name       string
+	Properties map[string]string
+	Containers []Container
+}
+
+type Container struct {
+	ID         string
+	Version    string
+	Repository string
+	Registry   string
+	Digest     string
+}
+
+type NodeInfo struct {
+	NodeName                string
+	KubeletVersion          string
+	ContainerRuntimeVersion string
+	OsImage                 string
+	Properties              map[string]string
+	KubeProxyVersion        string
+	Images                  []string
+}
